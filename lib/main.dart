bcd404c1fbf83e8646d7d214c97f6ea66c3e7382
Sihwan Kim <sihwan72@gmail.com>
2023-04-04 23:04:29 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,276 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+//-----------------------------------------------------------------------------------------
+var displayNumber = '0';
+var makeNumber = '' ;
+var selectedOperator = '+';
+var displayFontSize = 80.0 ;
+var pointExist = false ;
+
+dynamic firstNumber = 0 ;
+dynamic secondNumber = 0 ;
+
+var _disignPage = new DesignPage() ;
+//-----------------------------------------------------------------------------------------
+void main() 
+{
+	runApp(const CalculatorApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+//-----------------------------------------------------------------------------------------
+class CalculatorApp extends StatelessWidget 
+{
+  	const CalculatorApp({super.key});
+  
+	@override
+	Widget build(BuildContext context)  // This widget is the root of your application.
+	{
+		return MaterialApp
+    	(
+      		title: 'Flutter Demo',
+      		theme: ThemeData(primarySwatch: Colors.blue,),
+      		home: MainPage(),
+    	);
+  	}
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+//-----------------------------------------------------------------------------------------
+class MainPage extends StatefulWidget 
+{
+	@override
+  	DesignPage createState() => _disignPage ; // DesignPage();
 }
+//-----------------------------------------------------------------------------------------
+class DesignPage extends State<MainPage>  
+{
+  	@override
+	Widget build(BuildContext context)
+	{
+		return Scaffold
+		(
+			appBar: AppBar(title: Text('Calculator Program'),),
+			body: Column
+			(
+				crossAxisAlignment: CrossAxisAlignment.stretch,
+				children: <Widget>
+				[
+					Container
+					(
+						padding: EdgeInsets.all(30),
+						alignment: Alignment(1.0, 1.0),   // 내부 위젯의 위치를 우측 하단으로 설정 
+						color: Colors.black,
+    					child: displayValue(caption: '$displayNumber', fontsize: displayFontSize,),
+						height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.35,   // 화면의 35%를 차지하도록 설정
+					),
+					ButtonGroupWidget(),
+				],					
+			),
+			backgroundColor: Colors.black,   // 화면의 전테 뱌경색을 지정
+		);
+	}
+}  	
+//-----------------------------------------------------------------------------------------
+class displayValue extends StatelessWidget
+{
+	displayValue({super.key, required this.caption, required this.fontsize});
+	final String caption ;
+	final double fontsize ;
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+	@override
+	Widget build(BuildContext context)
+	{
+		return Text
+		(
+			'$caption', 
+			style: TextStyle(color: Colors.white, backgroundColor: Colors.black, fontSize: fontsize,),
+			textAlign: TextAlign.right,
+		) ;
+	}
 }
+//-----------------------------------------------------------------------------------------
+class CalButton extends StatelessWidget
+{
+	CalButton({super.key, required this.caption, required this.color, required this.buttonKind});
+	final String caption ;  // 립력된 버튼의 문자 
+	final Color color;      // 버튼의 색
+	final int buttonKind;   // 입력된 버튼의 기능(0:숫자, 1:연산, 2:기능)
+
+	@override
+	Widget build(BuildContext context)
+	{
+		return ElevatedButton
+		(
+			onPressed: () 
+			{ 
+				switch(buttonKind)
+				{
+					case 0 : _numberOnPressed(caption); break ;
+					case 1 : _operatorOnPressed(caption); break ;
+					case 2 : _resultOnPressed(caption); break ; 
+				}
+			},
+			style: ElevatedButton.styleFrom
+			(
+				backgroundColor: color, 
+				fixedSize: Size((MediaQuery.of(context).size.width/4)-30, (MediaQuery.of(context).size.width/4)-20), 
+				shape: const CircleBorder(),
+			),
+			child: Text('$caption', style: TextStyle(fontSize: 40,),),
+		);
+	}
+}
+//-----------------------------------------------------------------------------------------
+class ButtonGroupWidget extends StatelessWidget 
+{
+  	const ButtonGroupWidget({super.key});
+
+  	@override
+  	Widget build(BuildContext context) 
+	{
+    	return Table
+		(
+			border: TableBorder.all(),
+      		columnWidths: const <int, TableColumnWidth>
+			{
+        		0: FlexColumnWidth(),
+        		1: FlexColumnWidth(),
+        		2: FlexColumnWidth(),
+				3: FlexColumnWidth(),  
+      		},
+      		defaultVerticalAlignment: TableCellVerticalAlignment.middle,	
+      		children: <TableRow>
+			[
+        		TableRow
+				(
+					decoration: const BoxDecoration(color: Colors.black,),
+          			children: <Widget>
+					[
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: 'C', color: Colors.grey, buttonKind: 2,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '%', color: Colors.grey, buttonKind: 1,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '⇍', color: Colors.grey, buttonKind: 1,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '÷', color: Colors.orange, buttonKind: 1,),),
+          			],
+        		),
+        		TableRow
+				(
+					decoration: const BoxDecoration(color: Colors.black,),  
+          			children: <Widget>
+					[
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '7', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '8', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '9', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '×', color: Colors.orange, buttonKind: 1,),),
+          			],
+        		),
+				TableRow
+				(
+					decoration: const BoxDecoration(color: Colors.black,),
+          			children: <Widget>
+					[
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '4', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '5', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '6', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '−', color: Colors.orange,buttonKind: 1,),),
+          			],
+        		),
+				TableRow
+				(
+					decoration: const BoxDecoration(color: Colors.black,),
+          			children: <Widget>
+					[
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '1', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '2', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '3', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '+', color: Colors.orange, buttonKind: 1,),),
+          			],
+        		),			
+				TableRow
+				(
+					decoration: const BoxDecoration(color: Colors.black,),
+          			children: <Widget>
+					[
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '+/-', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '0', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '.', color: Color.fromARGB(255, 61, 61, 61), buttonKind: 0,),),
+						Padding(padding: EdgeInsets.all(5), child: CalButton(caption: '=', color: Colors.orange, buttonKind: 2,),),
+          			],
+        		),		
+      		],
+    	);
+  	}
+}
+//-----------------------------------------------------------------------------------------
+void _operatorOnPressed(String st)
+{
+	_resultOnPressed('=') ;
+	selectedOperator = st ;
+	firstNumber = double.parse(displayNumber) ;
+}
+//-----------------------------------------------------------------------------------------
+void _resultOnPressed(String st)
+{
+	if(st == 'C')		// clear input
+	{
+		selectedOperator = '+';
+		makeNumber = ''; 
+		displayNumber = '0' ;
+		firstNumber = 0 ;
+ 		secondNumber = 0 ;
+	}
+	else
+	{
+		secondNumber = double.parse(displayNumber) ;
+		makeNumber = '';
+
+		switch(selectedOperator)
+		{
+			case '+' : firstNumber = firstNumber + secondNumber ;	break ;
+			case '−' : firstNumber = firstNumber - secondNumber ;	break ;
+			case '×' : firstNumber = firstNumber * secondNumber ;	break ;
+			case '÷' : firstNumber = firstNumber / secondNumber ;	break ;
+		}
+
+		displayNumber = firstNumber.toString();
+		firstNumber = 0 ;
+		selectedOperator = '+';
+	}
+	 pointExist = false ;
+
+	if(displayNumber.length < 7) displayFontSize = 80.0 ;  // 글자 크기를 선택 
+	else displayFontSize = 50 ;
+
+	_disignPage.setState(() { displayNumber ;});  // 화면을 갱신한다. 
+}
+//-----------------------------------------------------------------------------------------
+void _numberOnPressed(String st)  		// 숫자키 입력 이벤트 함수 
+{
+	bool inputAdd = true ; 
+
+	if(makeNumber.length < 9)  // 숫자는 9자리 까지만 
+	{
+		if(st == '.') // 소숫점이 눌려졌을 경우 
+		{
+			if(makeNumber.isEmpty == true)
+			{ 
+				makeNumber += '0.';
+				inputAdd = false ;
+			}
+			else
+			{
+				if(pointExist == true) inputAdd = false ; // 소숫점이 없을 경우만 추가
+			}
+
+			pointExist = true ;
+		}
+		else if(st == '0' && makeNumber.isEmpty == true)  inputAdd = false;
+		
+		if(inputAdd == true) makeNumber += st ;
+
+		displayNumber = makeNumber ;
+
+		if(displayNumber.length < 7) displayFontSize = 80.0 ;  // 글자 크기를 선택 
+		else displayFontSize = 50 ;
+
+		_disignPage.setState(() { displayNumber ;});  // 화면을 갱신한다. 
+	}
+}
+//-----------------------------------------------------------------------------------------
