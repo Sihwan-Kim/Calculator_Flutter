0307eb40a870b36c53d9e09ac06edfe554daef30
Sihwan-Kim <sihwan72@gmail.com>
2023-04-14 14:13:41 +0900

    Update Model.dart

--- lib/Model.dart
+++ lib/Model.dart
@@ -5,14 +5,14 @@ import 'package:get/get.dart';
 class DisplayNumValue extends GetxController 
 {
 	String displayValue = '0' ;
-  double fontSize = 80.0 ;
+  	double fontSize = 80.0 ;
 
-  void displayNumber(String displayValue, double fontSize) 
+  	void displayNumber(String displayValue, double fontSize) 
 	{
     	this.displayValue = displayValue ;
     	this.fontSize = fontSize ;
-
-      update();
+		
+		update();
 	}
 }
 //-----------------------------------------------------------------------------------------
